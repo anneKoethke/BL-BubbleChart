aca857e3495cad3fe7b3a33e680572fa218e81bb
Anne Köthke <anne.koethke@stud.uni-regensburg.de>
2018-04-27 11:09:07 +0200

    add data
    make the data (json-file) available for the JS - doesn't work!

--- js/script.js
+++ js/script.js
@@ -4,5 +4,13 @@
     .attr('width', canvas_size)
     .attr('height', canvas_size);
   
-  
+  // um d3.queue zu ersetzen
+  // Quelle: https://stackoverflow.com/questions/49534470/d3-js-v5-promise-all-replaced-d3-queue (05-Apr-2018)
+  var file = ['../data/saison_16_17.json'];
+  Promise.all(file.map(url => d3.json(url))).then(function(entries) {
+    console.log(entries);
+    
+    //main(entries);
+  })
+
 })();
