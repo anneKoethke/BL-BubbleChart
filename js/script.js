657d25d7a0841e69be18dd8bb3a221b9124bf124
Anne Köthke <anne.koethke@stud.uni-regensburg.de>
2018-05-04 10:46:18 +0200

    JSON loads
    d3v5 uses Promise to load data

--- js/script.js
+++ js/script.js
@@ -1,23 +1,20 @@
 // ToDos: 
 // - BubbleChart mit Force-Simulation der Fouls, roten und gelben Karten aller Bundesligavereine einer Saison (16/17)
 // - Erweiterung der Darstellung über TABs im HTLM: 
-//    (1) Timeline für jeweils einen Verein: F,G,R über einzelne Spieltage
+//    (1) Timeline für jeweils einen Verein: F,G,R über einzelne Spieltage (HF, HY, HR, AF, AY, AR)
 //    (2) Auflistung der Vereine nebeneinander
 //    Vorbild: https://archive.nytimes.com/www.nytimes.com/interactive/2012/02/13/us/politics/2013-budget-proposal-graphic.html
 
-(function() {
-  var canvas_size = 500;
-  var canvas = d3.select('body').append('svg')
+var canvas_size = 500;
+var canvas = d3.select('body').append('svg')
     .attr('width', canvas_size)
     .attr('height', canvas_size);
-  
-  // um d3.queue zu ersetzen
-  // Quelle: https://stackoverflow.com/questions/49534470/d3-js-v5-promise-all-replaced-d3-queue (05-Apr-2018)
-  var file = ['../data/saison_16_17.json'];
-  Promise.all(file.map(url => d3.json(url))).then(function(entries) {
-    console.log(entries);
-    
-    //main(entries);
-  })
 
-})();
+// in d3v5 wird anscheinend JS Promise verwendet, um externe Daten einzulesen
+// siehe: https://bl.ocks.org/GerardoFurtado/f08993c9c729b0b3452ef1803ad9dcbf/c4b45c5acce6033085a667cbb7d34203d15de0f0 (04.05.18)
+// Quelle: https://stackoverflow.com/questions/49534470/d3-js-v5-promise-all-replaced-d3-queue (04.05.2018)
+var file = ["data/saison_16_17.json"];
+
+Promise.all(file.map(url => d3.json(url))).then(function(values) {
+    console.log(values)
+});
